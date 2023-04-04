b3e5b8203b7aa54204d9dfc822936ed1fec2db33
mo3aznasr <moaznasr@hotmail.com>
2023-04-03 17:54:49 -0700

    hellome

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,12 @@
 #include "main.h"
+
+/**
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
 	char *result = haystack, *fneedle = needle;
